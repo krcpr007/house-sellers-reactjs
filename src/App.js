292dae75f6e33a3a06c651739995cc27f5baf692
Rajan kumar <krcpr007@gmail.com>
2021-12-26 18:37:14 +0530

    Router sets

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'; 
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Offers from './pages/Offers';
+import ForgotPassword from './pages/ForgotPassword'; 
+import Explore from './pages/Explore';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore/>}/>
+          <Route path='/offers' element={<Offers/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/sign-in' element={<SignIn/>}/>
+          <Route path='/sign-up' element={<SignUp/>}/>
+          <Route path='/forgot-password' element={<ForgotPassword/>}/>
+        </Routes>
+        <NavBar/>
+        {/* Navbar */}
+      </Router>
+    </>
   );
 }
 
