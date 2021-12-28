919e328ae7e4b553b0296dc8164e6b7a4ece9d46
Rajan kumar <krcpr007@gmail.com>
2021-12-28 18:22:31 +0530

    Private compo added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Offers from './pages/Offers';
 import ForgotPassword from './pages/ForgotPassword'; 
 import Explore from './pages/Explore';
 import NavBar from './components/NavBar';
+import PrivateComponent from './components/PrivateComponent';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 function App() {
@@ -16,13 +17,14 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore/>}/>
           <Route path='/offers' element={<Offers/>}/>
+          <Route path='/profile' element={<PrivateComponent/>}>
           <Route path='/profile' element={<Profile/>}/>
+          </Route>
           <Route path='/sign-in' element={<SignIn/>}/>
           <Route path='/sign-up' element={<SignUp/>}/>
           <Route path='/forgot-password' element={<ForgotPassword/>}/>
         </Routes>
         <NavBar/>
-        {/* Navbar */}
       </Router>
       <ToastContainer />
     </>
