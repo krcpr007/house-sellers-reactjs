118821019cc735099789211c7abd8063b39a326e
Rajan kumar <krcpr007@gmail.com>
2021-12-28 18:19:56 +0530

    checking status learn from stack overfolow

--- src/components/PrivateComponent.jsx
+++ src/components/PrivateComponent.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import {Outlet ,Navigate} from 'react-router-dom'; 
-
-function PrivateComponent() {
-    const logedIn = false
-    return logedIn? <Outlet/> : <Navigate to='/sign-in'/>
+import { useAuthStatus } from '../hooks/useAuthStatus'
+import Spinner from './Spinner'
+export const PrivateComponent = ()=> {
+    const { loggedIn, checkingStatus } = useAuthStatus()
+    if(checkingStatus){
+        return <Spinner/>
+    }
+    return loggedIn? <Outlet/> : <Navigate to='/sign-in'/>
 }
 
 export default PrivateComponent
