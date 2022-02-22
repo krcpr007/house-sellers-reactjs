6760606d903f7b93328ed18bb5468f664c57dfc6
Rajan kumar <krcpr007@gmail.com>
2022-02-22 20:58:10 +0530

    new comp added

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import Offers from './pages/Offers';
 import ForgotPassword from './pages/ForgotPassword'; 
 import Explore from './pages/Explore';
 import NavBar from './components/NavBar';
+import Category from './pages/Category';
 import PrivateComponent from './components/PrivateComponent';
 import { ToastContainer } from 'react-toastify';
+import CreateListings  from './pages/CreateListings';
 import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
@@ -17,12 +19,14 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore/>}/>
           <Route path='/offers' element={<Offers/>}/>
+          <Route path='/category/:categoryName' element={<Category/>}/>
           <Route path='/profile' element={<PrivateComponent/>}>
           <Route path='/profile' element={<Profile/>}/>
           </Route>
           <Route path='/sign-in' element={<SignIn/>}/>
           <Route path='/sign-up' element={<SignUp/>}/>
           <Route path='/forgot-password' element={<ForgotPassword/>}/>
+          <Route path='/create-listing' element={<CreateListings/>}/>
         </Routes>
         <NavBar/>
       </Router>
